4a3b616934c3c800c6a27ac49f7563a78ed15882
MOUSSA-AI-DEV <essalhimoussa@gmail.com>
2025-11-15 19:50:38 +0100

    feat(modal): add assignment modal logic and input field handler

---
+++ asstes/js/app.js
@@ -0,0 +1,30 @@
+
+
+let addBtn = document.getElementById("btn-open-modal")
+let ModaleEmploye = document.getElementById("modal")
+
+
+addBtn.addEventListener("click", (e)=>{
+    ModaleEmploye.classList.remove("hidden")
+    console.log(ModaleEmploye)
+})  
+
+
+let empName = document.getElementById("empName")
+let empRole = document.getElementById("empName")
+let empEmail = document.getElementById("empEmail")
+let empPhone = document.getElementById("empPhone")
+let addExp = document.getElementById("addExp")
+let empPhoto = document.getElementById("empPhoto")
+
+
+
+
+
+
+
+
+
+let closeModal = document.getElementById("closeModal")
+let saveEmployee = document.getElementById("saveEmployee")
+
