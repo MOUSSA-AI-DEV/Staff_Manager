ded7fc70829aa8c00fb71162682a05ec808bca28
MOUSSA-AI-DEV <essalhimoussa@gmail.com>
2025-11-16 16:04:19 +0100

    feat: add Employee object structure and save logic to app.js

--- asstes/js/app.js
+++ asstes/js/app.js
@@ -1,17 +1,17 @@
-
+const STAFF = []
 
 let addBtn = document.getElementById("btn-open-modal")
 let ModaleEmploye = document.getElementById("modal")
 
 
-addBtn.addEventListener("click", (e)=>{
+addBtn.addEventListener("click", (e) => {
     ModaleEmploye.classList.remove("hidden")
     console.log(ModaleEmploye)
-})  
+})
 
 
 let empName = document.getElementById("empName")
-let empRole = document.getElementById("empName")
+let empRole = document.getElementById("empRole")
 let empEmail = document.getElementById("empEmail")
 let empPhone = document.getElementById("empPhone")
 let addExp = document.getElementById("addExp")
@@ -26,5 +26,41 @@ let empPhoto = document.getElementById("empPhoto")
 
 
 let closeModal = document.getElementById("closeModal")
+closeModal.addEventListener("click", (e) => {
+    ModaleEmploye.classList.add("hidden")
+
+})
 let saveEmployee = document.getElementById("saveEmployee")
 
+const inputs = [empName,
+    empRole,
+    empEmail,
+    empPhone,
+    empPhoto]
+let Employee = {}
+    saveEmployee.addEventListener("click",(e)=>{
+     let isValid=false
+        inputs.map((el)=>{
+            console.log([el])
+            console.log([el.value])
+            if(el.value.trim()!=""){
+                Employee[el.id]=el.value
+               console.log(el.id,el.value)
+               isValid=true
+            }
+            else{
+                alert(`  ${ el.id } not implement yet` )
+                isValid=false
+            }
+            console.log(Employee)
+        })
+       if(isValid){
+        STAFF.push(Employee)
+        console.log(STAFF)
+        inputs.forEach(element => {
+            element.value = ""
+        });
+}
+       
+    }
+)
