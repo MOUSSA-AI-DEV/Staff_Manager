44708976e56c983467b54d9808d081798f5a0ac7
MOUSSA-AI-DEV <essalhimoussa@gmail.com>
2025-11-16 18:57:38 +0100

    feat: add experience in modal and store it in localstorage   in file  app.js

--- asstes/js/app.js
+++ asstes/js/app.js
@@ -14,8 +14,11 @@ let empName = document.getElementById("empName")
 let empRole = document.getElementById("empRole")
 let empEmail = document.getElementById("empEmail")
 let empPhone = document.getElementById("empPhone")
-let addExp = document.getElementById("addExp")
 let empPhoto = document.getElementById("empPhoto")
+
+let addExp = document.getElementById("addExp")
+let expList = document.getElementById("expList")
+let expInput = document.getElementById("expInput");
 
 
 
@@ -32,38 +35,59 @@ closeModal.addEventListener("click", (e) => {
 })
 let saveEmployee = document.getElementById("saveEmployee")
 
-const inputs = [empName,
+const inputs = [
+    empName,
     empRole,
     empEmail,
     empPhone,
-    empPhoto]
+    empPhoto
+]
 let Employee = {}
-    saveEmployee.addEventListener("click",(e)=>{
-     let isValid=false
-        inputs.map((el)=>{
-            console.log([el])
-            console.log([el.value])
-            if(el.value.trim()!=""){
-                Employee[el.id]=el.value
-               isValid=true
-            }
-            else{
-                alert(`  ${ el.id } not implement yet` )
-                isValid=false
-            }
-        
-        })
-       if(isValid){
+saveEmployee.addEventListener("click", (e) => {
+    let isValid = false
+    inputs.map((el) => {
+        console.log([el])
+        console.log([el.value])
+        if (el.value.trim() != "") {
+            Employee[el.id] = el.value
+            isValid = true
+        }
+        else {
+            alert(`  ${el.id} not implement yet`)
+            isValid = false
+        }
+
+    })
+    if (isValid) {
         STAFF.push(Employee)
         console.log(STAFF)
         localStorage.setItem("STAFF", JSON.stringify(STAFF));
         inputs.forEach(element => {
-            element.value = ""
+            if (element.id !== "empRole")
+                element.value = ""
         });
-        }
-        let localStaff = JSON.parse(localStorage.getItem("STAFF"))
-        console.log("localstorage")
-        console.log(localStaff)    
     }
-    
+    let localStaff = JSON.parse(localStorage.getItem("STAFF"))
+    console.log("localstorage")
+    console.log(localStaff)
+}
+
 )
+// add to exp list 
+const arrayexpList = []
+addExp.addEventListener("click",(e)=>{
+ 
+    const el = expInput.value.trim(); 
+    if (el === "") return;
+    arrayexpList.push(el);
+    Employee["arrayexpList"] = [...arrayexpList]; 
+
+    const span = document.createElement("span");
+    span.className = "rounded-md border border-gray-300 bg-gray-300 w-auto inline-block p-2";
+    span.textContent = el;
+    expList.appendChild(span);
+     
+    expInput.value = "";
+
+})
+
